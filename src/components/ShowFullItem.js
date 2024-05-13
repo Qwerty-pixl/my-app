e97fd75a64fea2c0ed8cf78ad9063b1d193155b4
klaratokonova11 <148875652+Qwerty-pixl@users.noreply.github.com>
2024-05-13 14:53:47 +0600

    fix error

--- src/components/ShowFullItem.js
+++ src/components/ShowFullItem.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class ShowFullItem extends Component {
     render() {
         return (
             <div className="full-item">
                 <div>
-                    <img src={"./img/" + this.props.item.img} onClick={() => this.props.onShowItem(this.props.item)} />
+                    <img 
+                        src={"./img/" + this.props.item.img} 
+                        onClick={() => this.props.onShowItem(this.props.item)} 
+                        alt={this.props.item.alt} // Add the alt prop here
+                    />
                     <h2>{this.props.item.title}</h2>
                     <p>{this.props.item.desc}</p>
                     <b>{this.props.item.price}$</b>
                     <div className="add-to-card" onClick={() => this.props.onAdd(this.props.item)}>+</div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
-export default ShowFullItem
+export default ShowFullItem;
